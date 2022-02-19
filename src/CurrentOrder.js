72217df9936d3384a5f97a4ae8bcb3d9b9bd24c5
Rohit verma <rverma7300@gmail.com>
2022-02-19 12:13:24 +0530

    Update CurrentOrder.js

--- src/CurrentOrder.js
+++ src/CurrentOrder.js
@@ -1,7 +1,29 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { OrderContext } from "./App";
 const CurrentOrder = () => {
-  return <div>CurrentOrder</div>;
+  const { currentOrder, setCurrentOrder, pastOrders, setPastOrders } =
+    useContext(OrderContext);
+  return (
+    <>
+      <h1>Current-Order</h1>
+      {currentOrder.map((foodItem) => {
+        return (
+          <span>
+            <h2>{foodItem.title}</h2>
+            <h3>{foodItem.price}</h3>
+          </span>
+        );
+      })}
+      <button
+        onClick={() => {
+          setPastOrders(pastOrders.concat([currentOrder]));
+          setCurrentOrder([]);
+        }}
+      >
+        Confirm order
+      </button>
+    </>
+  );
 };
 
 export default CurrentOrder;
