1b22cdc83238065963737b42077d0d566e7a5fdc
Rohit verma <rverma7300@gmail.com>
2022-02-19 11:40:28 +0530

    creating Menu.js

---
+++ src/Menu.js
@@ -0,0 +1,13 @@
+import Card from "./components/Card";
+import data from "./Data";
+const Menu = (props) => {
+  return (
+    <>
+      {data.map((item) => {
+        return <Card key={item.id} food={item} />;
+      })}
+      ;
+    </>
+  );
+};
+export default Menu;
