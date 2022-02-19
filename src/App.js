e5bb381c3ca26c52a0bd3d1e411e178bd0a9296d
Rohit verma <rverma7300@gmail.com>
2022-02-19 11:22:48 +0530

    creating Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Card from "./component/Card";
 import "./App.css";
+import Navbar from "./component/Navbar";
 const App = () => {
   return (
     <>
+      <h1>Rohit</h1>
+      <Navbar />
       <Card />
     </>
   );
