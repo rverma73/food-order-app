dc934c73eead0e5c9cddefc147a58190409b6e4b
Rohit verma <rverma7300@gmail.com>
2022-02-19 11:57:08 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Menu from "./Menu";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import History from "./History";
 import CurrentOrder from "./CurrentOrder";
+export const OrderContext = React.createContext();
 const App = () => {
+  const [currentOrder, setCurrentOrder] = useState([]);
+  const [pastOrders, setPastOrders] = useState([]);
   return (
     <>
-      <Navbar />
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Menu />}></Route>
-          <Route exact path="/histroy" element={<History />}></Route>
-          <Route
-            exact
-            path="/current-orders"
-            element={<CurrentOrder />}
-          ></Route>
-        </Routes>
-      </div>
+      <OrderContext.Provider
+        value={{ currentOrder, setCurrentOrder, pastOrders, setPastOrders }}
+      >
+        <Navbar />
+
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Menu />}></Route>
+            <Route exact path="/histroy" element={<History />}></Route>
+            <Route
+              exact
+              path="/current-orders"
+              element={<CurrentOrder />}
+            ></Route>
+          </Routes>
+        </div>
+      </OrderContext.Provider>
     </>
   );
 };
